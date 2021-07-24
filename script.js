6240b5b63bab15c8542911d500f7f8cf5e894a0c
Emily <emilyllewelyn@gmail.com>
2021-07-24 12:06:38 +0100

    form entry to object

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
 
-  
+function handleSubmit(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const value = Object.fromEntries(data.entries());
+  value.percent = data.get('percent');
+  console.log({ value });
+  console.log(value.bill);
+  let bill = value.bill;
+  let percent = value.percent;
+  let people = value.noOfPeople;
+  tipCalc(bill, percent, people);
+}
 
+function tipCalc(bill, percent, people) {
+  console.log(bill + ' this is from tipcalc');
+}
