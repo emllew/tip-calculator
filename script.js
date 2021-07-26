45ec19bb137d37a1b5fbd8310e417d6816307d84
Emily <emilyllewelyn@gmail.com>
2021-07-26 15:15:13 +0100

    reset button and can't be zero fn

--- script.js
+++ script.js
@@ -4,23 +4,44 @@ form.addEventListener('submit', handleSubmit);
 let tipPerPerson = document.querySelector('.tipPerPerson');
 let totalPerPerson = document.querySelector('.totalPerPerson');
 
+let numberOfPeople = document.querySelector('.numberOfPeople');
+let bill = document.querySelector('.bill');
+
+const resetBtn = document.querySelector('.reset');
+resetBtn.addEventListener('click', reset);
+
 function handleSubmit(event) {
   event.preventDefault();
   const data = new FormData(event.target);
   const value = Object.fromEntries(data.entries());
   value.percent = data.get('percent');
-  console.log({ value });
-  console.log(value.bill);
   let bill = value.bill;
   let percent = value.percent;
   let people = value.noOfPeople;
-  tipCalc(bill, percent, people);
+
+  if (people == 0) {
+    const cantBeZero = document.createElement('h2');
+    cantBeZero.setAttribute('class', 'CBZ');
+    cantBeZero.innerText = "Can't be zero";
+    numberOfPeople.appendChild(cantBeZero);
+    delete value.bill;
+    delete value.percent;
+    delete value.noOfPeople;
+  } else {
+    tipCalc(bill, percent, people);
+  }
+}
+
+function reset(value) {
+  tipPerPerson.textContent = '£0.00';
+  totalPerPerson.textContent = '£0.00';
+  form.reset();
 }
 
 function tipCalc(bill, percent, people) {
   let tip = ((bill / 100) * percent).toFixed(2);
   let tipsEach = tip / people;
   let totalBillEach = bill / people;
-  tipPerPerson.textContent = '£' + tip;
+  tipPerPerson.textContent = '£' + tipsEach.toFixed(2);
   totalPerPerson.textContent = '£' + (totalBillEach + tipsEach).toFixed(2);
 }
